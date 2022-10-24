d39c4143f07813906099212d55c355668a8b6367
John Hewitt <john.hewitt64@gmail.com>
2022-10-23 21:58:54 -0700

    add test reporting

---
+++ local_scripts/webtext/test_mauve_metrics_kmeans.sh
@@ -0,0 +1,110 @@
+#!/usr/bin/env bash
+
+# TODO: set `njobs` (number of jobs to run at one) and `data_dir`
+
+source local_scripts/parallelize.sh
+njobs=4 # TODO
+cmds=""
+
+
+set -u  # x: stack trace
+export MKL_NUM_THREADS=1
+export NUMEXPR_NUM_THREADS=1
+export OMP_NUM_THREADS=1
+
+export CUDA_VISIBLE_DEVICES=""
+export DISABLE_TQDM=True
+
+# options
+datasplit="test"
+dataset="webtext"
+#model_name="gpt2-large"
+#max_len=${1}
+
+# Default args
+if [ ${dataset} == "webtext" ]; then
+    data_dir="./data/webtext"  # TODO
+else
+    data_dir="UNKNOWN dataset ${dataset}"
+    exit 100
+fi
+
+discretization="kmeans_l2"
+kmeans_num_clusters=500
+
+#for max_len in 1024 512 256 128
+#for max_len in 1024 #512 256 128
+for max_len in 1024 512 256 128
+do
+#for generate_seed in 10 11 12 13 14 15
+#for generate_seed in 0 1 2 3 4 5
+#for generate_seed in 100 101 102 103 104
+#for generate_seed in 200 201 202 203 204
+#for generate_seed in 1 2 3 4 5
+for generate_seed in 5 4 3 2 1
+#for generate_seed in 3 4 5
+do
+#for model_name in "gpt2" "gpt2-medium" "gpt2-large" "gpt2-xl"
+#for model_name in "gpt2" "gpt2-medium" "gpt2-large"
+#for model_name in "gpt2-xl" "gpt2-large"
+for model_name in "gpt2" "gpt2-medium" "gpt2-large" "gpt2-xl"
+do
+
+args="  --data_dir ${data_dir} --model_name ${model_name} "
+sn="${discretization}_${kmeans_num_clusters}_${model_name}_${max_len}"
+
+options="${args} --datasplit ${datasplit} --discretization ${discretization} --device -1"
+options="${options} --kmeans_num_clusters ${kmeans_num_clusters}"
+options="${options} --generate_seed ${generate_seed} --seed 1234"
+options="${options} --use_large_feats --max_len ${max_len} --kmeans_explained_var 0.9"
+
+##################
+# basic
+##################
+# eta
+#for e in 0.00005 0.0001 0.0003 0.0006 0.001
+#for e in 5e-5 2e-5 5e-6 1e-6
+for h in 0.002 0.0009 0.0006 0.0003
+do
+    cmds="$cmds ; time python -u compute_mauve_metrics.py ${options} --generation_type basic --eta ${h} > outputs/${dataset}_${model_name}/outs/basic/${datasplit}_mauve_${sn}_h_${h}_seed${generate_seed} 2>&1 "
+done
+
+# epsilon
+for e in 0.0003 0.0006 0.0009
+#for e in 5e-5 2e-5 5e-6 1e-6
+#for e in 0.004 0.002 0.0009 0.0006 0.0003
+do
+    cmds="$cmds ; time python -u compute_mauve_metrics.py ${options} --generation_type basic --epsilon ${e} > outputs/${dataset}_${model_name}/outs/basic/${datasplit}_mauve_${sn}_e_${e}_seed${generate_seed} 2>&1 "
+done
+
+# nucleus
+for p in 0.89 0.9 0.95
+#for p in 0.89
+#for p in 0.9 0.905 0.91 0.92 0.925 0.93 0.935 0.94 0.945 0.95 0.955 0.96 0.965 0.97 0.975 0.98 0.985 0.99 0.995
+do
+    cmds="$cmds ; time python -u compute_mauve_metrics.py ${options} --generation_type basic --top_p ${p} > outputs/${dataset}_${model_name}/outs/basic/${datasplit}_mauve_${sn}_p_${p}_seed${generate_seed} 2>&1 "
+done
+
+## top-k
+#for k in 1
+#do
+#    cmds="$cmds ; time python -u compute_mauve_metrics.py ${options} --generation_type basic --top_k ${k} > outs/basic/mauve_${sn}_k_${k}_seed${generate_seed} 2>&1 "
+#done
+#
+## temperature
+#for t in 1.0
+#do
+#    cmds="$cmds ; time python -u compute_mauve_metrics.py ${options} --generation_type basic --temp ${t} > outs/basic/mauve_${sn}_t_${t}_seed${generate_seed} 2>&1 "
+#done
+
+done # model_name
+done # seed
+done # length
+
+############ DONE ###########
+
+echo "executing..."
+date
+set +u # for parallel exec to work (unbound variables)
+f_ParallelExec $njobs "$cmds"
+date
--- report.py
+++ report.py
@@ -4,6 +4,7 @@ import numpy as np
 
 argp = argparse.ArgumentParser()
 argp.add_argument('--just_best', default=None)
+argp.add_argument('--test', default=False)
 args = argp.parse_args()
 
 model_sizes = ('', '-medium', '-large', '-xl')
@@ -14,9 +15,10 @@ eta_hyps = (0.004, 0.002, 0.0009, 0.0006, 0.0003)
 
 top_p_seeds_valid = (1, 2, 3, 4, 5)
 epsilon_seeds_valid = (1, 2, 3, 4, 5)
-eta_seeds_valid = (200, 201, 202, 203, 204)
+eta_seeds_valid = (200, 201, 202, 203, 204, 1, 2, 3, 4, 5)
 
 lengths = (128, 256, 512, 1024)
+
 
 path = 'outputs/webtext_gpt2{}/metrics/basic/mauve_L{}_valid_p{}_k0_t1.0_e{}_h{}_seed{}_kmeans_l2_500_0.9.p'
 
@@ -34,9 +36,10 @@ def results_helper(hyps, seeds, typ):
           resolved_path = path.format(model_size, length, 1.0, 0.0, hyp, seed)
         result = pickle.load(open(resolved_path, 'rb'))[0]
         results[hyp].append(result)
+        #print(typ, hyp, seed)
       except:
         pass
-  #print(results)
+  print(results)
   results = {hyp: (round(np.mean(results[hyp]),4), np.std(results[hyp]), len(results[hyp])) for hyp in results}
   if args.just_best:
     print(list(sorted(results.items(), key=lambda x: 100 if np.isnan(x[1][0]) else -x[1][0]))[0])
@@ -46,8 +49,28 @@ def results_helper(hyps, seeds, typ):
 
 for model_size in model_sizes:
   for length in lengths:
+    if args.test:
+      path = 'outputs/webtext_gpt2{}/metrics/basic/mauve_L{}_test_p{}_k0_t1.0_e{}_h{}_seed{}_kmeans_l2_500_0.9.p'
+      if model_size == '':
+        top_p_hyps = (0.9,)
+        eta_hyps = (0.002,)
+        epsilon_hyps = (0.0006,)
+      if model_size == '-medium':
+        top_p_hyps = (0.89,)
+        eta_hyps = (0.002,)
+        epsilon_hyps = (0.0009,)
+      if model_size == '-large':
+        top_p_hyps = (0.95,)
+        eta_hyps = (0.0006,)
+        epsilon_hyps = (0.0003,)
+      if model_size == '-xl':
+        top_p_hyps = (0.95,)
+        eta_hyps = (0.0003,)
+        epsilon_hyps = (0.0003,)
     print()
     print(model_size if model_size != '' else 'small', length)
     results_helper(top_p_hyps, top_p_seeds_valid,  'p')
     results_helper(epsilon_hyps, epsilon_seeds_valid, 'e')
     results_helper(eta_hyps, eta_seeds_valid, 'h')
+
+
